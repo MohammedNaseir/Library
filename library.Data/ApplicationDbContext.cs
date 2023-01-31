4bd1dabf3bb923b8c3b025cada78e049dcd58dd4
Mohammed <Mohammed@DESKTOP-LD7J4I6>
2023-01-31 15:07:27 +0200

    Add project files.

---
+++ library.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace library.Data
+{
+    public class libraryDbContext : IdentityDbContext
+    {
+        public libraryDbContext(DbContextOptions<libraryDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
